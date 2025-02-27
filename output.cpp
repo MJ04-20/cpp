7d2e9f55d354c8cd6ca508394ccbc59ae5f49dee
Mrutyunjay Lodhi <2004lodhi@gmail.com>
2025-02-28 00:28:52 +0530

    started learning c++

---
+++ output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    std::cout << "Let's start "<< " \n";
+    std::cout << "Let's start \n meow";
+
+    std::cout << "Let's start "<< std::endl;
+    std::cout << "Let's start \n meow";
+    return 0;
+
+}
